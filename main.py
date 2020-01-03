179721adb6556eb64e2cda28015ddf3f2b1553a5
Valeriy Osipov <info@vosipov.com>
2020-01-04 01:16:53 +0300

    Show points on map

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import os
 
+import folium
+import geopandas as gpd
 import pandas as pd
+from decouple import config
 
 geo = pd.read_excel(os.path.join('data', 'geo.xlsx'))
 # geo_comment = pd.read_excel(os.path.join('data', 'geo.xlsx'))
-print(geo)
+
+API_KEY = config('YANDEX_API_KEY')
+locations = gpd.tools.geocode('Moscow, Russia', provider='Yandex', api_key=API_KEY)
+loc = locations.geometry[0]
+
+folium_map = folium.Map(location=[loc.y, loc.x])
+
+for index, row in geo.iterrows():
+    row['x'], row['y']
+    break
+
+folium_map
