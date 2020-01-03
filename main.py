3a19a02476fca7c603b4d8a3d48666736282e7e9
Valeriy Osipov <info@vosipov.com>
2020-01-03 17:17:05 +0300

    Read data files

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello')
+import os
+
+import pandas as pd
+
+geo = pd.read_excel(os.path.join('data', 'geo.xlsx'))
+# geo_comment = pd.read_excel(os.path.join('data', 'geo.xlsx'))
+print(geo)
